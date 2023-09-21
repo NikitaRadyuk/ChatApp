a8fb36cfe8b42611fbc224cb998d1386ee1036da
NikitaRadyuk <radyuknikita02@gmail.com>
2023-09-21 23:05:48 +0300

    create ValidationException.java

---
+++ src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/core/exceptions/ValidationException.java
@@ -0,0 +1,23 @@
+package by.it.jd2.Mk_JD2_103_23.chatApp.core.exceptions;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException() {
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    public ValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
