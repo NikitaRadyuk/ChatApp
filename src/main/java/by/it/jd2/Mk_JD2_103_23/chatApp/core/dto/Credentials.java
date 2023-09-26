55d47a21d0702e427471c92d587c2a5469752e09
L1BER2Y <xerdan.99@mail.ru>
2023-09-26 16:58:40 +0300

    Added dto Credentials for login password params from loginServlet

---
+++ src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/core/dto/Credentials.java
@@ -0,0 +1,58 @@
+package by.it.jd2.Mk_JD2_103_23.chatApp.core.dto;
+
+import java.util.Objects;
+
+public class Credentials {
+    private String login;
+    private String password;
+
+    public Credentials() {
+    }
+
+    public Credentials(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Credentials that = (Credentials) o;
+
+        if (!Objects.equals(login, that.login)) return false;
+        return Objects.equals(password, that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = login != null ? login.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Credentials{" +
+                "login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
