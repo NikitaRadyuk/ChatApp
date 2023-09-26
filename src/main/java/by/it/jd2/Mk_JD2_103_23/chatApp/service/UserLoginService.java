c8e52de0ee9d75d3e8c8947cdc95014b60c88a6b
L1BER2Y <xerdan.99@mail.ru>
2023-09-26 16:08:39 +0300

    login servlet, IUserLoginService, UserLoginService (no logic)

--- src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/service/UserLoginService.java
+++ src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/service/UserLoginService.java
@@ -3,13 +3,24 @@ package by.it.jd2.Mk_JD2_103_23.chatApp.service;
 import by.it.jd2.Mk_JD2_103_23.chatApp.core.dto.User;
 import by.it.jd2.Mk_JD2_103_23.chatApp.service.api.IUserLoginService;
 
+import java.util.List;
+
 /**
  * Реализация авторизации пользователя
  */
 public class UserLoginService implements IUserLoginService {
+    /**
+     * авторизация пользователя
+     * @param
+     */
     @Override
-    public void check(User user) {
+    public void login() {
 
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
     }
 }
 
--- src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/service/api/IUserLoginService.java
+++ src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/service/api/IUserLoginService.java
@@ -2,9 +2,13 @@ package by.it.jd2.Mk_JD2_103_23.chatApp.service.api;
 
 import by.it.jd2.Mk_JD2_103_23.chatApp.core.dto.User;
 
+import java.util.List;
+
 /**
  * Интерфейс, сервис для авторизации пользователя
  */
 public interface IUserLoginService {
-    void check(User user);
+    void login();
+
+    List<User> getAllUsers();
 }
