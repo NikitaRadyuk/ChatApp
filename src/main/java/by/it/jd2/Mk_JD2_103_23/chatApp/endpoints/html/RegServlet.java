1d20a58aa6679e5696342df59a470579f69d583b
NikitaRadyuk <radyuknikita02@gmail.com>
2023-09-27 23:05:37 +0300

    loginServlet.java+signUp.jsp logic

--- src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/endpoints/html/RegServlet.java
+++ src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/endpoints/html/RegServlet.java
@@ -29,6 +29,8 @@ public class RegServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
         req.getRequestDispatcher("/ui/signUp.jsp").forward(req,resp);
     }
 
