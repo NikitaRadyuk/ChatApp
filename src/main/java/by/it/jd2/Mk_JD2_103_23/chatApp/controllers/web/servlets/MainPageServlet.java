d5c7e83bf943ac539996564e10f3b534111ce2c4
L1BER2Y <xerdan.99@mail.ru>
2023-09-29 12:40:54 +0300

    main page ul pattern fix

--- src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/controllers/web/servlets/MainPageServlet.java
+++ src/main/java/by/it/jd2/Mk_JD2_103_23/chatApp/controllers/web/servlets/MainPageServlet.java
@@ -9,11 +9,10 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/ui/", loadOnStartup = 1)
+@WebServlet(urlPatterns = "/api/", loadOnStartup = 1)
 public class MainPageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         req.getRequestDispatcher("/ui/index.jsp").forward(req,resp);
     }
     @Override
